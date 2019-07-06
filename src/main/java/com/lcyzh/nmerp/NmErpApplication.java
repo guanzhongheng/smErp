033e7c4fee97fd6db5bc4efe686edf3fac6a9cb6
guanzhongheng <373284103@qq.com>
2019-07-07 00:55:52 +0800

    pom

--- src/main/java/com/lcyzh/nmerp/NmErpApplication.java
+++ src/main/java/com/lcyzh/nmerp/NmErpApplication.java
@@ -1,5 +1,6 @@
 package com.lcyzh.nmerp;
 
+import org.apache.log4j.BasicConfigurator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class NmErpApplication {
 
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
 		SpringApplication.run(NmErpApplication.class, args);
 	}
 
