a82fc7fdc4e73264733ea932386b5dee3d219261
chenhao <1573974366@qq.com>
2019-07-22 02:29:54 +0800

    fi bug

--- src/main/java/com/lcyzh/nmerp/service/impl/TProdPlanDetailServiceImpl.java
+++ src/main/java/com/lcyzh/nmerp/service/impl/TProdPlanDetailServiceImpl.java
@@ -8,7 +8,6 @@ import com.lcyzh.nmerp.entity.*;
 import com.lcyzh.nmerp.model.vo.ProdPlanDetailVo;
 import com.lcyzh.nmerp.service.TProdPlanDetailService;
 import com.lcyzh.nmerp.utils.IdGen;
-import com.sun.xml.internal.bind.v2.TODO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
