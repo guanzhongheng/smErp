fc1786e59eab511d514bb9ce3d21ccf35f196a66
daxingxing <544195627@qq.com>
2019-07-24 19:08:24 +0800

    重新提交

--- src/test/java/com/lcyzh/nmerp/NmErpApplicationTests.java
+++ src/test/java/com/lcyzh/nmerp/NmErpApplicationTests.java
@@ -1,15 +1,21 @@
 package com.lcyzh.nmerp;
 
+import com.google.common.io.Files;
 import com.lcyzh.nmerp.model.vo.OutStockVo;
 import com.lcyzh.nmerp.service.TOutStockService;
+import org.apache.commons.io.Charsets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class NmErpApplicationTests {
 
 	@Autowired
@@ -21,6 +27,17 @@ public class NmErpApplicationTests {
 //        outStockVo.setOutCode("1000");
 
 
+	}
+
+
+	@Test
+	public void reader() throws IOException {
+		File file = new File("/Users/lijinku/Documents/smErp/src/test/java/barCodes.txt");
+		List<String> lines = Files.readLines(file, Charsets.UTF_8);
+		for (String line:
+			 lines) {
+			System.out.println(line);
+		}
 	}
 
 }
