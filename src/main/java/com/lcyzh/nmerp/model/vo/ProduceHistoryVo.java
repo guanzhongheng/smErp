0f2602bf0ba623b5ea99f8ed3cb18691dcc47728
guanzhongheng <373284103@qq.com>
2019-12-06 19:46:43 +0800

    修改提交

---
+++ src/main/java/com/lcyzh/nmerp/model/vo/ProduceHistoryVo.java
@@ -0,0 +1,11 @@
+package com.lcyzh.nmerp.model.vo;
+
+/**
+ * 项目名称：nm-erp
+ * 类 名 称：ProduceHistoryVo
+ * 类 描 述：TODO
+ * 创建时间：2019/12/6 11:52 AM
+ * 创 建 人：guan
+ */
+public class ProduceHistoryVo {
+}
